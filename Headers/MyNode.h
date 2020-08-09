0c0aaaa804b5fa3934abba49a393b4f4e3ac34a6
Sun2sky <64154196+Sun2sky126@users.noreply.github.com>
2020-08-09 11:52:34 +0800

    Update MyNode.h
    I rewrite the class considering MyNode.h

--- Headers/MyNode.h
+++ Headers/MyNode.h
@@ -1,56 +1,62 @@
 /*
- *MyNode.h
+ *MyList.h
  *Created on 8/9/2020
  *Completed on ...
  *By S.H.
- *The declaration of the class "MyNode"
+ *The declaration of the class "MyList"
  */
 
-#ifndef _MYNODE_H_
-#define _MYNODE_H_
+#ifndef _MYLIST_H_
+#define _MYLIST_H_
+
+#include"MyNode.h"
 
 template<class T>
-class MyNode {
+class MyList {
 
     protected:
 
-    //The value part of a node
-    T value;
+    //The fisrt node
+    MyNode<T> * headNode;
 
-    //The pointer part of a node
-    MyNode * pointer;
+    //The length of the whole list
+    int length;
 
     public:
 
-    MyNode();
-    MyNode(T _value_);
-    MyNode(T _value_, MyNode * _pointer_);
+    MyList();
+    MyList(T value);
 
-    ~MyNode();
+    ~MyList();
 
-    //Set the value of the node
-    //INPUT: The value to be set
-    //OUTPUT: NONE
-    void setValue(T _value_);
+    //Add a node
+    //INPUT: The value to be added, the order of the location
+        //If order is n, the value will be added next to n
+    //OUTPUT: 1 for success and 0 for failure
+    int addNode(T value, int order);
 
-    //Set the pointer
-    //INPUT: The pointer to be set
-    //OUTPUT: NONE
-    void setPointer(MyNode * _pointer_);
+    //Delete a node
+    //INPUT: The order of the node to be deleted
+    //OUTPUT: 1 for success and 0 for failure
+    int deleteNode(int order);
 
-    //Get the value
+    //Search a node
+    //INPUT: The order of the node to be searched
+    //OUTPUT: The pointer to the node
+    MyNode* searchNode(int order);
+
+    //Get the length of the list
     //INPUT: NONE
-    //OUTPUT: The value
-    T getValue();
+    //OUTPUT: The length
+    int getLength();
 
-    //Get the pointer
-    //INPUT: NONE
-    //OUTPUT: The pointer
-    MyNode* getPointer();
+    //Swap two nodes
+    //INPUT: The order_1, the order_2
+    //OUTPUT: 1 for success and 0 for failure
+    int swapNodes(int order_1, int order_2);
 };
 
-#endif // _MYNODE_H_
+#endif // _MYLIST_H_
 
 
-
-/************END OF THE FILE**************/
+/**************END OF THE FILE*****************/
