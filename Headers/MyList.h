0025144ae8e59613256549afd1b2777c570197d1
Sun2sky <64154196+Sun2sky126@users.noreply.github.com>
2020-08-09 11:54:06 +0800

    Update MyList.h
    I rewrite the header file considering MyNode.h

--- Headers/MyList.h
+++ Headers/MyList.h
@@ -1,52 +1,62 @@
 /*
  *MyList.h
- *Created on 8/8/2020
+ *Created on 8/9/2020
  *Completed on ...
- *By SUN Hao
+ *By S.H.
  *The declaration of the class "MyList"
  */
 
 #ifndef _MYLIST_H_
 #define _MYLIST_H_
 
+#include"MyNode.h"
+
 template<class T>
 class MyList {
 
     protected:
 
-    //The node in the list
-    T node;
+    //The fisrt node
+    MyNode<T> * headNode;
 
-    //The pointer to the next node
-    MyList * pointer;
+    //The length of the whole list
+    int length;
 
     public:
 
     MyList();
-    MyList(T _node_);
+    MyList(T value);
 
     ~MyList();
 
-    // //Create a new list(start from "this pointer")
-    // int createMyList();
+    //Add a node
+    //INPUT: The value to be added, the order of the location
+        //If order is n, the value will be added next to n
+    //OUTPUT: 1 for success and 0 for failure
+    int addNode(T value, int order);
 
-    //Append a node to the end
-    int appendNode(T _node_);
+    //Delete a node
+    //INPUT: The order of the node to be deleted
+    //OUTPUT: 1 for success and 0 for failure
+    int deleteNode(int order);
 
-    //Insert a node to a certain location
-    int insertNode(T _node_, int order);
+    //Search a node
+    //INPUT: The order of the node to be searched
+    //OUTPUT: The pointer to the node
+    MyNode* searchNode(int order);
 
-    //Add a node to the head
-    int addNode(T _node_);
+    //Get the length of the list
+    //INPUT: NONE
+    //OUTPUT: The length
+    int getLength();
 
-    //Delete the node at a certain location
-    int deleteNode(T _node_, int order);
-
-    //Delete the whole list
-    int del();
-
-    //Search the node
-    T* searchNode(int order);
+    //Swap two nodes
+    //INPUT: The order_1, the order_2
+    //OUTPUT: 1 for success and 0 for failure
+    int swapNodes(int order_1, int order_2);
 };
 
-#endif //_MYLIST_H_
+#endif // _MYLIST_H_
+
+
+/**************END OF THE FILE*****************/
